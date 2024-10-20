31a3ee8ec5c35fafebee69727de834ec97d951e5
nowon9159 <nowon9159@gmail.com>
2024-10-20 11:52:00 +0900

    Feat: Install git, Modify helm values archi

--- kube_install/main_node.sh
+++ kube_install/main_node.sh
@@ -16,6 +16,18 @@ sudo systemctl start containerd
 
 sudo systemctl status containerd
 
+## Install git
+sudo apt-get install -y git
+
+sudo git clone https://github.com/nowon9159/homelab_src.git
+
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
 
 ## IPtables, sysctl
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
