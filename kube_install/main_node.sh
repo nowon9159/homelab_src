1d35238dd0f0473b6913889501f8be57ab7ded7b
nowon9159 <nowon9159@gmail.com>
2024-10-20 12:22:20 +0900

    Feat: Modify version of kubelet,adm,ctl

--- kube_install/main_node.sh
+++ kube_install/main_node.sh
@@ -74,7 +74,7 @@ sudo mkdir -m 755 /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.25/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.25/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubelet==1.25 kubeadm==1.25 kubectl==1.25
+sudo apt-get install -y kubelet=1.25.13-1.1 kubeadm=1.25.13-1.1 kubectl=1.25.13-1.1
 sudo apt-mark hold kubelet kubeadm kubectl
 
 ## Init kubeadm
