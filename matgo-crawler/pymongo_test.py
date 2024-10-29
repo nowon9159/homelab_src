ab1b60f710a55dbbaccc95db3304b93eda96d457
nowon9159 <nowon9159@gmail.com>
2024-10-29 15:22:43 +0900

    Feat: Init pymongo_test

---
+++ matgo-crawler/pymongo_test.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+client = MongoClient('127.0.0.1', 45243) # minikube service mongodb --url
+db_matgo = client.matgo
+
+
+all_metadata = list(db_matgo.metadata.find({}))
+print(all_metadata[0])
+
+# DML
+# Insert
+
+# db_matgo.metadata.insert_one(
+#     {
+#         'name':'bobby','age':21
+#         }
+#     )
+
+# db_matgo.metadata.insert_many(
+#     [
+#         {
+#         'name':'bobby','age':21
+#         },
+#         {
+#         'name':'john','age':23
+#         },
+#     ]
+#     )
