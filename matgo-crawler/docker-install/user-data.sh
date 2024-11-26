c9eded1544091511d5880a038a5ecc6e623a0a9e
nowon9159 <nowon9159@gmail.com>
2024-11-26 10:13:41 +0900

    Feat: mongodb env 변경

--- matgo-crawler/docker-install/user-data.sh
+++ matgo-crawler/docker-install/user-data.sh
@@ -10,9 +10,9 @@ apt upgrade -y
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 ## Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings -y
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
@@ -21,13 +21,13 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin -y
 
 # Docker Compose plugin 설치
-sudo apt-get update
-sudo apt-get install docker-compose-plugin
+sudo apt-get update -y
+sudo apt-get install docker-compose-plugin -y
 
 sudo DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 sudo mkdir -p $DOCKER_CONFIG/cli-plugins
@@ -38,10 +38,10 @@ sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 sudo docker compose version
 
 # docker project, docker dir 생성
-sudo mkdir -r /app/docker-compose/matgo
-sudo mkdir -r /app/docker-compose/matgo/frontend
-sudo mkdir -r /app/docker-compose/matgo/backend
-sudo mkdir -r /app/docker-compose/matgo/mongodb
-sudo mkdir -r /app/docker-compose/matgo/gitlab
+sudo mkdir -p /app/docker-compose/matgo
+sudo mkdir -p /app/docker-compose/matgo/frontend
+sudo mkdir -p /app/docker-compose/matgo/backend
+sudo mkdir -p /app/docker-compose/matgo/mongodb
+sudo mkdir -p /app/docker-compose/matgo/gitlab
 
 # 
