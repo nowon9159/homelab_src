2930d00adfdf16103119367c728b29f5c0946df1
nowon9159 <nowon9159@gmail.com>
2024-12-11 23:16:56 +0900

    Feat: UserAgent 라이브러리 기반으로 변경

--- matgo-crawler/src/store_info/settings.py
+++ matgo-crawler/src/store_info/settings.py
@@ -1,10 +1,13 @@
 # 설정 파일
+from fake_useragent import UserAgent
+USER_AGENT = UserAgent(platforms="pc", browsers="chrome")
+
 
 class Settings:
     def __init__(self):
         self.BASE_URL = f"https://map.naver.com/restaurant/list"
         self.COORD_URL = "https://gps.aply.biz/"
-        self.USER_AGENT = 'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        self.USER_AGENT = USER_AGENT
         self.KEYWORD = "양천향교역 김치찌개" #test
 
 # asdfasdfsssss
