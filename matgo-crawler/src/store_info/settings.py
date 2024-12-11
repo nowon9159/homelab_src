6ed99a3d57fae0cfe08b2b5845d654643d604256
jundory <ykwanlee98@naver.com>
2024-12-11 17:13:52 +0900

    Fix: settings.py git ignore 해제

---
+++ matgo-crawler/src/store_info/settings.py
@@ -0,0 +1,10 @@
+# 설정 파일
+
+class Settings:
+    def __init__(self):
+        self.BASE_URL = f"https://map.naver.com/restaurant/list"
+        self.COORD_URL = "https://gps.aply.biz/"
+        self.USER_AGENT = 'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        self.KEYWORD = "양천향교역 김치찌개" #test
+
+# asdfasdfsssss
