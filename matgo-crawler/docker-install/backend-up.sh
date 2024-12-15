66761dc68c9dea244ee6793048701a4921c63dbe
nowon9159 <nowon9159@gmail.com>
2024-12-15 20:56:47 +0900

    Fix: docker build path 수정

--- matgo-crawler/docker-install/backend-up.sh
+++ matgo-crawler/docker-install/backend-up.sh
@@ -1,5 +1,5 @@
 git -C /root/matgo/src/back origin pull origin master
 
-docker build -t matgo-back:latest /root/matgo/src/back/matgo-back/src/.
+docker build -t matgo-back:latest /root/matgo/src/back/matgo-back/.
 
 docker compose up -d backend
--- matgo-crawler/docker-install/frontend-up.sh
+++ matgo-crawler/docker-install/frontend-up.sh
@@ -1,5 +1,5 @@
 git -C /root/matgo/src/front origin pull origin master
 
-docker build -t matgo-front:latest /root/matgo/src/front/matgo-front/src/.
+docker build -t matgo-front:latest /root/matgo/src/front/matgo-front/.
 
 docker compose up -d frontend
