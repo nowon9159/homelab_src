55bc4d284c47b3fff8add7076cc5ff6eec256f95
nowon9159 <nowon9159@gmail.com>
2024-12-15 20:42:23 +0900

    Fix: backend build path 수정

--- matgo-crawler/docker-install/backend-up.sh
+++ matgo-crawler/docker-install/backend-up.sh
@@ -1,5 +1,5 @@
 git -C /root/matgo/src/back origin pull origin master
 
-docker build -t matgo-back:latest /root/matgo/src/back/matgo-back/sr``
+docker build -t matgo-back:latest /root/matgo/src/back/matgo-back/src/.
 
 docker compose up -d backend
