21c92f37c8fb7b93a7552e43b438c67ae52abc08
nowon9159 <nowon9159@gmail.com>
2024-12-23 10:03:09 +0900

    Feat: crawler build 경로 변경

--- matgo/matgo-crawler/docker-install/crawler-up.sh
+++ matgo/matgo-crawler/docker-install/crawler-up.sh
@@ -1,6 +1,6 @@
 #git -C /root/matgo/git_src/crawler/crawler pull origin main
 
-cp -r /home/clouflake/ai_model /root/matgo/git_src/crawler/src
-docker build -t crawler:latest /root/matgo/git_src/crawler/src/.
+cp -r /home/clouflake/ai_model /root/matgo/src
+docker build -t crawler:latest /root/matgo/src/.
 
 docker compose up -d crawler
